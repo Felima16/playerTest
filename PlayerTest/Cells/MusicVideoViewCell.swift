ce284861254b7c9eed810a8e34280c14091b0d9b
Fernanda de Lima <limade.f@gmail.com>
2018-01-17 09:00:14 -0200

    Iniciado os trabalhos

---
+++ PlayerTest/Cells/MusicVideoViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MusicVideoCollectionViewCell.swift
+//  PlayerTest
+//
+//  Created by Fernanda de Lima on 17/01/2018.
+//  Copyright © 2018 Empresinha. All rights reserved.
+//
+
+import UIKit
+
+class MusicVideoViewCell: UITableViewCell {
+    
+}
---
+++ Pods/Target Support Files/Pods-PlayerTestUITests/Pods-PlayerTestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PlayerTestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PlayerTestUITests
+@end
